7ee77a2c75593543e9bfad88f6662862db8e70cf
Lance Belen <s3944846@student.rmit.edu.au>
2023-11-13 13:59:48 +1100

    Setup base code for website

---
+++ lance-belen/src/fragments/Header.js
@@ -0,0 +1,9 @@
+import '../App.css';
+
+function Header() {
+  return (
+    <></>
+  );
+}
+
+export default Header;
---
+++ lance-belen/src/fragments/Navbar.js
@@ -0,0 +1,9 @@
+import '../App.css';
+
+function Navbar() {
+  return (
+    <></>
+  );
+}
+
+export default Navbar;
