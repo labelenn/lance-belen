c0fea4e0bd397342159c155d4bb8a2470df5fb8d
Lance Belen <s3944846@student.rmit.edu.au>
2023-11-28 15:07:49 +1100

    Fixed navlinks

--- lance-belen/src/fragments/Header.js
+++ lance-belen/src/fragments/Header.js
@@ -4,9 +4,9 @@ import logo from '../images/logo.png';
 function Header() {
   return (
     <div className = "header">
-      <a href = "/lance-belen/about-me"><h1>Lance</h1></a>
-      <a href = "/lance-belen/about-me"><img id = "header-logo" src = {logo} alt = "logo"></img></a>
-      <a href = "/lance-belen/about-me"><h1>Belen</h1></a>
+      <a href = "/lance-belen"><h1>Lance</h1></a>
+      <a href = "/lance-belen"><img id = "header-logo" src = {logo} alt = "logo"></img></a>
+      <a href = "/lance-belen"><h1>Belen</h1></a>
     </div>
   );
 }
--- lance-belen/src/fragments/Navbar.js
+++ lance-belen/src/fragments/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
     <div className = "navbar">
       <ul>
         <li><NavLink to = "/lance-belen/projects" style = {{textDecoration: "none", fontSize: 18}}>Projects</NavLink></li>
-        <li><NavLink to = "/lance-belen/about-me" style = {{textDecoration: "none", fontSize: 18}}>About Me</NavLink></li>
+        <li><NavLink to = "/lance-belen" style = {{textDecoration: "none", fontSize: 18}}>About Me</NavLink></li>
         <li><NavLink to = "/lance-belen/resume" style = {{textDecoration: "none", fontSize: 18}}>Resume</NavLink></li>
         <li><NavLink to = "/lance-belen/photography" style = {{textDecoration: "none", fontSize: 18}}>Photography</NavLink></li>
       </ul>
