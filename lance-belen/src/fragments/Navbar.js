1d48ed9c6f2dc5e9b3489595fbea29ed801df2bf
Lance Belen <s3944846@student.rmit.edu.au>
2024-01-19 13:37:48 +1100

    Implementation of burger nav for mobilemode

--- lance-belen/src/fragments/Navbar.js
+++ lance-belen/src/fragments/Navbar.js
@@ -1,9 +1,41 @@
 import '../App.css';
 import { NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function Navbar() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      var nav = document.querySelector(".navbar ul");
+      setWindowWidth(window.innerWidth);
+      if (windowWidth <= 600) {
+        nav.style.display = "block";
+      }
+      else {
+        nav.style.display = "flex";
+      }   
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, []);
+
+  function displayLinks() {
+    var nav = document.querySelector(".navbar ul");
+    if (nav.style.display === "block") {
+      nav.style.display = "none";
+    } 
+    else {
+      nav.style.display = "block";
+    }
+  }
+
   return (
     <div className = "navbar">
+      <div onClick = {displayLinks} className = "burger-nav"></div>
       <ul>
         <li><NavLink to = "/lance-belen/projects" style = {{textDecoration: "none", fontSize: 18}}>Projects</NavLink></li>
         <li><NavLink to = "/lance-belen/about-me" style = {{textDecoration: "none", fontSize: 18}}>About Me</NavLink></li>
