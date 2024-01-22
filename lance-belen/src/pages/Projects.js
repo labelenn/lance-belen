eac1e4a7521eabfb042c0a1159fb1e57aed8d90e
Lance Belen <s3944846@student.rmit.edu.au>
2024-01-22 13:14:58 +1100

    Some design changes to header and navbar, added a project

--- lance-belen/src/pages/Projects.js
+++ lance-belen/src/pages/Projects.js
@@ -2,6 +2,7 @@ import '../App.css';
 import FWD from '../videos/FWD_Project_Demo.mp4'
 import SEPT from '../videos/SEPT_Project_Demo.mp4'
 import PS1 from '../videos/PS1_Project_Demo.mp4'
+import SalaryPredictions from '../videos/Salary-Predictions.mp4'
 
 function Projects() {
   return (
@@ -10,9 +11,10 @@ function Projects() {
       <h1>My Projects</h1>
       <div className = "project-info">
         <div className = "project-details">
-          <h3>Cinema Website Application</h3>
+          <a href = "https://labelenn.github.io/Cinema-Website-Project/"><h3>Cinema Website Application</h3></a>
           <h4>A full stack web application that I developed for a university project.</h4>
           <h4>Technology Stack used: React, HTML, CSS, NodeJS, Express, Sequelize, MySQL, GraphQL</h4>
+          <h4><a id = "project-link" href = "https://labelenn.github.io/Cinema-Website-Project/">Check it out</a></h4>
         </div>
         <video className = "project-video" autoPlay muted loop >
           <source src = {FWD} type = "video/mp4"></source>
@@ -22,9 +24,6 @@ function Projects() {
       <hr></hr>
 
       <div className = "project-info">
-        <video className = "project-video" autoPlay muted loop height = "300px">
-          <source src = {SEPT} type = "video/mp4"></source>
-        </video>
         <div className = "project-details">
           <h3>Supermarket Website Application</h3>
           <h4>A full stack web application for tracking the cheapest items across different available retailers to give customers the best options.</h4>
@@ -34,6 +33,25 @@ function Projects() {
           </h4>
           <h4>Technology Stack used: React, HTML, CSS, MySQL, Java, Spring Boot.</h4> 
         </div>
+        <video className = "project-video" autoPlay muted loop height = "300px">
+          <source src = {SEPT} type = "video/mp4"></source>
+        </video>
+      </div>
+
+      <div className = "project-info">
+        <div className = "project-details">
+          <h3>Data Analysis and Modeling</h3>
+          <h4>A data analysis and modeling project for predicting salaries of different Data Science jobs.</h4>
+          <h4>This project focuses on applying statistical modeling techniques by presenting our chosen problem, 
+              providing a detailed analysis of the data, and a proper application of model selection strategies.
+          </h4>
+          <h4>For this end-to-end project, my team and I went through data preparation, data exploration, data visualisation, and statistical modeling.</h4>
+          <h4>Technology Stack used: Python, NumPy, Pandas, SQL, Excel, Jupyter Notebook</h4>
+          <h4><a id = "project-link" href = "https://github.com/labelenn/Salary-Prediction">Check it out</a></h4>
+        </div>
+        <video className = "project-video" autoPlay muted loop height = "300px">
+          <source src = {SalaryPredictions} type = "video/mp4"></source>
+        </video>
       </div>
 
       <hr></hr>
