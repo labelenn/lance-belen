9a4adf1d1568660802dfc9955458eb4df19e6ca3
Lance Belen <s3944846@student.rmit.edu.au>
2024-02-01 21:15:56 +1100

    Design changes to header, navbar, font

--- lance-belen/src/fragments/Header.js
+++ lance-belen/src/fragments/Header.js
@@ -6,12 +6,7 @@ function Header() {
   return (
     <>
     <div className = "header">
-      <NavLink to = "/lance-belen"><h1>Lance</h1></NavLink>
       <NavLink to = "/lance-belen"><img id = "header-logo" src = {logo} alt = "logo"></img></NavLink>
-      <NavLink to = "/lance-belen"><h1>Belen</h1></NavLink>
-    </div>
-    <div className = "sub-header">
-      
     </div>
     </>
   );
--- lance-belen/src/pages/Home.js
+++ lance-belen/src/pages/Home.js
@@ -1,22 +1,5 @@
 import '../App.css';
-import me from '../images/profile-photo1.jpg';
-import java from '../images/skills/java.png';
-import python from '../images/skills/python.png';
-import cpp from '../images/skills/c++.png';
-import sql from '../images/skills/sql-server.png';
-import html from '../images/skills/html-5.png';
-import css from '../images/skills/css-3.png';
-import nodejs from '../images/skills/nodejs.png';
-import npm from '../images/skills/npm.png';
-import react from '../images/skills/react.png';
-import graphQL from '../images/skills/graphQL.png';
-import git from '../images/skills/git.png';
-import lightroom from '../images/skills/lightroom.png';
-import photoshop from '../images/skills/photoshop.png';
-import user from '../images/user.png';
-import team from '../images/team.png';
-import hobbies from '../images/hobbies.png';
-
+import me from '../images/profile-photo4.jpg';
 
 function Home() {
   return (
@@ -24,7 +7,7 @@ function Home() {
     <div className = "home">
       <img src = {me} alt = "my profile"></img>
       <div className = "home-intro">
-        <h4>Hi! I'm Lance.</h4>
+        <h4>Hi! I'm Lance Belen.</h4>
         <h4>Welcome to my personal website.</h4>
         <h4>I am an aspiring Software Engineer currently studying at RMIT University Australia.</h4>
         <h4>Feel free to explore the tabs above to get to know me more.</h4>
